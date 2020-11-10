ed3795084998ac3846811a0a480f2e361bc778ab
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2020-11-10 11:13:50 +0100

    parallax

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Coming soon
+window.addEventListener('scroll', function(e){
+    
+    const target = document.querySelectorAll(".scroll");
+
+
+    var scrolled =  window.pageYOffset;
+
+    var index = 0, length = target.length;
+    for (index; index < length; index++) {
+        var pos = window.pageYOffset * target[index].dataset.rate;
+        target[index].style.transform = 'translate3d('+pos+'px, 0px, 0px)';
+    }
+
+    
+});
